21e5a83ba856f1716433037ce5d2195acffb2e44
x <x@Laptop-a009036c.localdomain>
2021-08-13 02:28:12 -0400

    fix time

---
+++ precache-manifest.dc621df87d958a08741dd8756b4413c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30829ebfdaa0cafb13a367bae8cf7951",
+    "url": "./index.html"
+  },
+  {
+    "revision": "44ae7a47a6694111b6ae",
+    "url": "./static/css/2.5b039a8e.chunk.css"
+  },
+  {
+    "revision": "799606e6dc73051bec66",
+    "url": "./static/css/main.6a9a0f53.chunk.css"
+  },
+  {
+    "revision": "44ae7a47a6694111b6ae",
+    "url": "./static/js/2.98c1fa4d.chunk.js"
+  },
+  {
+    "revision": "c97921bcb254fdbb489a6e27a9964fe8",
+    "url": "./static/js/2.98c1fa4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "799606e6dc73051bec66",
+    "url": "./static/js/main.68e86760.chunk.js"
+  },
+  {
+    "revision": "028762adaf48b66839f6",
+    "url": "./static/js/runtime-main.08baa623.js"
+  }
+]);
